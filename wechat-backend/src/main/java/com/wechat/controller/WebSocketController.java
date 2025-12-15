c0d9a0254c36803f2f5024f462e1ab2209f4526c
hoobang <admin@hoobang.cn>
2025-12-15 14:29:26 +0800

    清理未使用代码：删除未引用的 WebSocketMessage，移除控制器未用导入，保持构建通过

--- wechat-backend/src/main/java/com/wechat/controller/WebSocketController.java
+++ wechat-backend/src/main/java/com/wechat/controller/WebSocketController.java
@@ -5,9 +5,7 @@ import com.wechat.entity.GroupMessage;
 import com.wechat.service.MessageService;
 import com.wechat.service.GroupService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
